4748ff27208782c4eca49bd6de9bcd77d96f501e
Daniel Redly <dredly@gmail.com>
2022-12-01 00:10:42 +0000

    Trying to get back the access token

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,21 +1,22 @@
 package controllers
 
-import configuration.ApiVariables.{CLIENT_ID, REDIRECT_URI}
+import com.google.common.io.BaseEncoding.base64
+import configuration.ApiVariables.{CLIENT_ID, CLIENT_SECRET, REDIRECT_URI}
+import play.api.libs.json.{JsDefined, JsUndefined}
 import play.api.libs.ws._
 import play.api.mvc._
 
 import javax.inject._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents, ws: WSClient) extends BaseController {
 
   val AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+  val TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token"
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val params = Map(
       "response_type" -> "code",
@@ -49,12 +50,45 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents, w
   }
 
   def callback(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    // TODO: redirect to error page if code not present
     val authCode = request.getQueryString("code")
     authCode match {
       case Some(code) =>
-        println(s"The code is $code")
+        val body = Map(
+          "grant_type" -> "authorization_code",
+          "code" -> code,
+          "redirect_uri" -> REDIRECT_URI
+        )
+
+        val authString = "Basic " + base64.encode(s"client_id:$CLIENT_SECRET".getBytes)
+
+        val headers = Map(
+          "Authorization" -> authString,
+          "Content-Type" -> "application/x-www-form-urlencoded"
+        )
+
+        val response = ws.url(TOKEN_ENDPOINT)
+          .withHttpHeaders(headers.toSeq:_*)
+          .post(body)
+          .map {
+            response => response.json \ "access_token"
+          }
+
+        response.onComplete {
+          case Success(authToken) =>
+            authToken match {
+              case JsDefined(authToken) =>
+                println(s"The auth token is $authToken")
+                Ok(views.html.callback())
+              case JsUndefined() =>
+                println()
+                throw new Exception("Could not find desired JSON field")
+            }
+          case Failure(exception) =>
+            throw exception
+        }
+
         Ok(views.html.callback())
+
       case None =>
         println(s"No code was returned!")
         NotFound(views.html.callback())
